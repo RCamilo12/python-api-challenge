2b9f16f27e97654551ed71a0307ff85708e6a283
RCamilo12 <rozocamilo22@gmail.com>
2024-05-09 21:10:36 -0500

    Final code

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "381750526c01c670be46f58d78b611e4"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "578e2240565343d4882adfbcf3b090f1"
